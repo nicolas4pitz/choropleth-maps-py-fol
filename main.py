b9c19883846e4483dba1415b9c7e2a868044adc7
nicolas4pitz <nicolas2004pitz@gmail.com>
2025-05-14 17:04:11 -0300

    inspecionando o banco de dados

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ import pandas as pd
 import geopandas as gpd
 import folium
 import numpy as np
+
+ff_df = pd.read_csv('https://raw.githubusercontent.com/programminghistorian/jekyll/gh-pages/assets/choropleth-maps-python-folium/fatal-police-shootings-data.csv', parse_dates = ['date'])
+
+ff_df.info()
